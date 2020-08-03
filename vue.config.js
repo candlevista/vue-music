0a3c9eb7933f13cdbe050a8d93241ff07c9d7322
火箭快餐 <864994121@qq.com>
2020-08-03 22:23:29 +0800

    no message

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("src", resolve("src"))    
+      .set("common", resolve("src/common"))  
+      .set("components", resolve("src/components"))
+  },
+}
